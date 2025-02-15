cf28b66141963cb6d04e28d80b8cdb32093b2f12
Ibrahim Koteish <ibrahim.koteish@feverup.com>
2025-02-15 12:23:12 +0100

    Initial Commit

---
+++ FinancialFlow/FinancialFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialFlowApp.swift
+//  FinancialFlow
+//
+//  Created by Ibrahim Koteish on 15/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FinancialFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
